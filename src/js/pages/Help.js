9c0cb35204aa22061510341852e4cffe1914c342
SaladRaider <peterkangveerman@gmail.com>
2016-06-29 22:05:00 -0700

    Added instructions in help page that tells users how to use new export buttons

--- src/js/pages/Help.js
+++ src/js/pages/Help.js
@@ -9,12 +9,12 @@ export default class HomePage extends React.Component {
 					<p>
 						This seating chart generator uses a form of a genetic algorithm. If you would like to learn more about genetic algorithms, this is a good read: <a target="_blank" href="https://www.doc.ic.ac.uk/~nd/surprise_96/journal/vol1/hmw/article1.html">https://www.doc.ic.ac.uk/~nd/surprise_96/journal/vol1/hmw/article1.html</a>
 						<br /><br />
-						The algorithm used in this generator is using mutation and selection only, without crossover. This should result in a parallel, noise-tolerant, hill-climbing result. Keep this in mind when adjusting any variables that affect the algorithm. I might try using crossover later on to see if it produces better results.
+						The algorithm used in this generator is using mutation and selection only, without crossover.
 						Steps to generate a seating chart:
 						<ol>
-							<li><p>Load a student roster .csv file and test score .csv file. Make sure that the order of the test scores in the test scores file corresponds to the same order of students in the student roster.</p></li>
+							<li><p>Load a student roster .csv file and test score .csv file. You may also load a "history[...].csv" file if you have generated one from a previous seating chart. Make sure that the order of the test scores in the test scores file corresponds to the same order of students in the student roster.</p></li>
 							<li>
-								<p>Edit the genetic algorithm factors. Here are a description of some: </p>
+								<p>Edit the genetic algorithm factors. The default values should be sufficient. However, if you can try to change them to optimize your results. Here are a description of some: </p>
 								<p>Genetic Algorithm Independent Variables:</p>
 								<ul>
 									<li><p>Population Size: This is the fixed populatoin size for all the generated seating charts.</p></li>
@@ -25,10 +25,12 @@ export default class HomePage extends React.Component {
 							<li>
 								<p>Edit the genetic weights. The higher the weight, the more that factor will be prioritized.</p>
 							</li>
-							<li><p>Press "Sort Students" and wait untill the timeout time is over. You will be notified once the time is up.</p></li>
+							<li><p>Press "Sort Students" and wait until the timeout time is over.</p></li>
 							<li><p>Scroll down to see the generated seating chart.</p></li>
+							<li><p>Press "Export History" to export a "history[...].csv file.</p></li>
+							<li><p>Press "Export Table" to export the seating chart .xls file.</p></li>
 						</ol>
-						Right now, the seating chart lists the students name, their gender, whether or not they requested to be in front, and their test score. The graphical representation of the seating chart right now is just temporary, and will be imporved later on.
+						The seating chart lists the students name, their gender, and their test score. Those who wish to sit in front are bolded. Those who have already sat in the 4<sup>th</sup> column are highlighted in red. The exported .xls seating chart will <b>only</b> have the names of the students with no extra information.
 					</p>
 				</div>
 			</div>
